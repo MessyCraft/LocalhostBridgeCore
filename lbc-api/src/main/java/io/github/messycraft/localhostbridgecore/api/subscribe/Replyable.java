95d9cee6201f4728c992988f96d9d6cc72f67527
Mreate <1705482458@qq.com>
2025-11-13 03:48:03 +0800

    tt

--- lbc-api/src/main/java/io/github/messycraft/localhostbridgecore/api/subscribe/Replyable.java
+++ lbc-api/src/main/java/io/github/messycraft/localhostbridgecore/api/subscribe/Replyable.java
@@ -6,7 +6,7 @@ public interface Replyable {
     /**
      * 回复一条消息。只有最先回复的内容会被发送，此后将无效。
      *
-     * @throws UnsupportedOperationException 如果具体内容附带的{@code needReply}为false
+     * @throws UnsupportedOperationException 如果具体内容附带的{@code needReply}为<tt>false</tt>
      * @return 是否回复成功。超时或多次回复都会导致失败。
      */
     boolean reply(String content);
